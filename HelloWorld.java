a0b31d51fbe86b322bb42890066d4812044fbd3c
Chiemezie Ezechukwu <cezechu1@email.cpcc.edu>
2024-09-16 12:31:41 -0400

    Output Update
    I added an output line to the code

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class HelloWorld {
     public static void main(String[] args) {
        System.out.println("HEllo World!");
+       System.out.println("Program modified, but able to revert to original version using GitHub Desktop."); 
 }
 
 }
